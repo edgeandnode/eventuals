8058c463ea463a7fadb1c0441d84be6125ead3ee
Zac Burns <That3Percent@gmail.com>
2021-07-09 21:01:25 -0500

    Test: Separated and improved eventual tests

--- tests/eventual.rs
+++ tests/eventual.rs
@@ -1,34 +1,83 @@
 use eventuals::*;
-use std::time::Duration;
 use tokio::{join, test};
 
-// TODO: Much more sophisticated tests are needed.
 #[test]
-async fn it_works() {
-    let (mut writer, eventual) = Eventual::<u32>::new();
+async fn dropped_writer_closes() {
+    let (writer, eventual) = Eventual::<u32>::new();
     let mut read_0 = eventual.subscribe();
-    let read_x = eventual.subscribe();
-    let mut read_1 = eventual.subscribe();
-    let mut read_2 = eventual.subscribe();
+    drop(writer);
+    assert_eq!(read_0.next().await, Err(Closed));
+}
 
+#[test]
+async fn can_observe_value_written_after_subscribe() {
+    let (mut writer, eventual) = Eventual::new();
+    let mut read_0 = eventual.subscribe();
     writer.write(5);
-    assert_eq!(read_0.next().await.unwrap(), 5);
-    drop(read_x);
+    assert_eq!(read_0.next().await, Ok(5));
+}
 
-    let r0 =
-        tokio::spawn(async move { read_0.next().await.unwrap() + read_0.next().await.unwrap() });
+#[test]
+async fn can_observe_value_written_before_subscribe() {
+    let (mut writer, eventual) = Eventual::new();
+    writer.write(5);
+    let mut read_0 = eventual.subscribe();
+    assert_eq!(read_0.next().await, Ok(5));
+}
 
-    let r1 = tokio::spawn(async move {
-        writer.write(10);
-        tokio::time::sleep(Duration::from_millis(10)).await;
-        let next = read_1.next();
-        writer.write(8);
-        tokio::time::sleep(Duration::from_millis(10)).await;
-        next.await.unwrap()
+#[test]
+async fn only_most_recent_value_is_observed() {
+    let (mut writer, eventual) = Eventual::new();
+    let mut read_0 = eventual.subscribe();
+    writer.write(5);
+    writer.write(10);
+    assert_eq!(read_0.next().await, Ok(10));
+}
+
+#[test]
+async fn drop_doesnt_interfere() {
+    // TODO: A dbg! was used to verify that the subscriber count goes to 0 and
+    // there is no leak. A unit test may be required to make that test permanent.
+    let (mut writer, eventual) = Eventual::new();
+    let mut read_0 = eventual.subscribe();
+    let mut read_1 = eventual.subscribe();
+    writer.write(5);
+    writer.write(10);
+    assert_eq!(read_0.next().await, Ok(10));
+    drop(read_0);
+    writer.write(1);
+    assert_eq!(read_1.next().await, Ok(1));
+}
+
+#[test]
+async fn can_message_pass() {
+    let (mut writer_a, eventual_a) = Eventual::<u32>::new();
+    let (mut writer_b, eventual_b) = Eventual::<u32>::new();
+
+    let mut eventual_a = eventual_a.subscribe();
+    let mut eventual_b = eventual_b.subscribe();
+
+    let b = tokio::spawn(async move {
+        let mut sum = 0;
+        while let Ok(v) = eventual_a.next().await {
+            sum += v;
+            writer_b.write(v + 1);
+        }
+        sum
     });
 
-    let (r0, r1) = join!(r0, r1);
-    assert_eq!(r0.unwrap(), 18);
-    assert_eq!(r1.unwrap(), 8);
-    assert_eq!(read_2.next().await, Err(Closed));
+    let a = tokio::spawn(async move {
+        writer_a.write(0);
+        let first = eventual_b.next().await.unwrap();
+        writer_a.write(first + 1);
+        let second = eventual_b.next().await.unwrap();
+        writer_a.write(second + 1);
+        assert_eq!(eventual_b.next().await, Ok(5));
+        drop(writer_a);
+        assert_eq!(eventual_b.next().await, Err(Closed));
+    });
+
+    let (a, b) = join!(a, b);
+    assert!(a.is_ok());
+    assert_eq!(b.unwrap(), 6);
 }
